7464e4cd4a0f91fd613762f955549c8856bbdfaf
lizihang <lzhsmart@163.com>
2022-11-23 15:58:04 +0800

    update

--- dm-gateway/src/main/java/com/dm/study/cloud/route/RequestPathGatewayFilterFactory.java
+++ dm-gateway/src/main/java/com/dm/study/cloud/route/RequestPathGatewayFilterFactory.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 import java.util.List;
-
 /**
  * <p>标题：请求路径过滤器</p>
  * <p>功能：自定义路由过滤器</p>
@@ -24,50 +23,48 @@ import java.util.List;
  */
 @Component
 public class RequestPathGatewayFilterFactory extends AbstractGatewayFilterFactory<RequestPathGatewayFilterFactory.Config> {
+	private static final Logger logger = LoggerFactory.getLogger(RequestPathGatewayFilterFactory.class);
 
-    private static final Logger logger = LoggerFactory.getLogger(RequestPathGatewayFilterFactory.class);
+	public RequestPathGatewayFilterFactory() {
+		super(Config.class);
+	}
 
-    public RequestPathGatewayFilterFactory() {
-        super(Config.class);
-    }
+	@Override
+	public GatewayFilter apply(Config config) {
+		return (exchange, chain) -> {
+			String path = exchange.getRequest().getPath().toString();
+			logger.info("路由过滤器：本次请求地址：{}；配置的参数：[name={},path={}]", path, config.getName(), config.getPath());
+			return chain.filter(exchange);
+		};
+	}
 
-    @Override
-    public GatewayFilter apply(Config config) {
-        return (exchange, chain) -> {
-            String path = exchange.getRequest().getPath().toString();
-            logger.info("路由过滤器：本次请求地址：{}；配置的参数：[name={},path={}]", path, config.getName(), config.getPath());
-            return chain.filter(exchange);
-        };
-    }
+	/**
+	 * 简化配置
+	 * @return
+	 */
+	@Override
+	public List<String> shortcutFieldOrder() {
+		return Arrays.asList("name", "path");
+	}
 
-    /**
-     * 简化配置
-     *
-     * @return
-     */
-    @Override
-    public List<String> shortcutFieldOrder() {
-        return Arrays.asList("name", "path");
-    }
+	public static class Config {
+		private String name;
+		private String path;
 
-    public static class Config {
-        private String name;
-        private String path;
+		public String getName() {
+			return name;
+		}
 
-        public String getName() {
-            return name;
-        }
+		public void setName(String name) {
+			this.name = name;
+		}
 
-        public void setName(String name) {
-            this.name = name;
-        }
+		public String getPath() {
+			return path;
+		}
 
-        public String getPath() {
-            return path;
-        }
-
-        public void setPath(String path) {
-            this.path = path;
-        }
-    }
+		public void setPath(String path) {
+			this.path = path;
+		}
+	}
 }
